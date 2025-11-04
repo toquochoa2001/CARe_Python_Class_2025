5f29b60806c614c65145046da5cab0a486e16ab9
toquochoa <toquochoa2001@gmail.com>
2025-11-04 17:59:30 +0100

    Update ex_0_intermediate_runfile.py

--- week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
+++ week_0/ex_0_intermediate/ex_0_intermediate_runfile.py
@@ -1,1 +1,15 @@
+import math
+ini_str = input("initial cell number: ")
+ini = float(ini_str)
+final_str = input("final cell number: ")
+final = float(final_str)
+time_str = input("enter your time elapsed: ")
+time = float(time_str)
+if ini <= 0 or final <= 0:
+    print("Error: No Cell counts")
+elif time <= 0:
+    print("Error: No Time elapsed")
+else:
+    growth_rate= (math.log(final) - math.log(ini)) / time
+print(f"growth rate is : {growth_rate}")
 #YOUR CODE FOR EX_0 INTERMEDIATE HERE
